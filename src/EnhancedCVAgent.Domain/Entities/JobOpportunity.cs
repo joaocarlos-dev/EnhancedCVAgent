768f8b4e069b4a6bcd847847e2fee56712387ec2
joao.carlos <joao.carlos@sebsa.com.br>
2026-05-21 16:37:00 -0300

    feat: Add Job Opportunity Entity

---
+++ src/EnhancedCVAgent.Domain/Entities/JobOpportunity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnhancedCVAgent.Domain.Entities
+{
+    public class JobOpportunity
+    {
+        public string Url { get; }
+        public string Title { get; }
+        public string CompanyName { get; }
+        public string Description { get; }
+        private readonly List<string> _jobSkillRequirement = [];
+        private readonly List<string> Responsibilities = [];
+        public string SeniorityLevel { get; }
+        public DateOnly ExtractedAt { get; }
+    }
+}
