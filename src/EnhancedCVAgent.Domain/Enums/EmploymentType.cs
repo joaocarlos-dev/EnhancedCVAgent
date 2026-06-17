98bee89487f82304e3951eee8202a4fd067883b2
joao.carlos <joao.carlos@sebsa.com.br>
2026-06-17 16:17:56 -0300

    feat: add EmploymentType enum

---
+++ src/EnhancedCVAgent.Domain/Enums/EmploymentType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public enum EmploymentType
+{
+    NaoInformado,
+    CLT,
+    PJ,
+    Cooperado,
+    Estagio,
+    Freelancer,
+    Temporario
+}
