b7c30a0bdaa8200aa34de94b8952766d9d3b5c05
joao.carlos <joao.carlos@sebsa.com.br>
2026-06-22 09:58:10 -0300

    feat: add jobSkillRequirement

---
+++ src/EnhancedCVAgent.Domain/ValueObjects/JobOpportunity/JobSkillRequirement.cs
@@ -0,0 +1,24 @@
+﻿using EnhancedCVAgent.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace EnhancedCVAgent.Domain.ValueObjects.JobOpportunity
+{
+    public sealed record JobSkillRequirement
+    {
+        public JobSkillRequirement(string skill, string? skillExperienceTime)
+        {
+            if (string.IsNullOrEmpty(skill)){
+                throw new DomainValidationException("Skill cannot be empty.");
+            }
+
+            Skill = skill;
+            SkillExperienceTime = skillExperienceTime;
+        }
+        public string Skill { get; }
+        public string? SkillExperienceTime { get; }
+
+    }
+}
