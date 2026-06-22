fe09f6b159d39e2152b9fc6c616fcecb01c7e80a
joao.carlos <joao.carlos@sebsa.com.br>
2026-06-22 10:00:29 -0300

    feat: add responsability valueObject

---
+++ src/EnhancedCVAgent.Domain/ValueObjects/JobOpportunity/Responsability.cs
@@ -0,0 +1,19 @@
+﻿using EnhancedCVAgent.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnhancedCVAgent.Domain.ValueObjects.JobOpportunity
+{
+    public sealed record Responsability
+    {
+        public Responsability(string description)
+        {
+            if (string.IsNullOrEmpty(description))
+            {
+                throw new DomainValidationException("Responsability description cannot be empty");
+            }
+        }
+        public string Description { get; }
+    }
+}
