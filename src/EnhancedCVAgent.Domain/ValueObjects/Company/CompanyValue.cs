13b4422902a544b59839edc88e8ed36976c40a5c
joaocarlos-dev <joaohenriquejc@hotmail.com>
2026-06-24 15:15:09 -0300

    feat: add companyValue ValueObject

---
+++ src/EnhancedCVAgent.Domain/ValueObjects/Company/CompanyValue.cs
@@ -0,0 +1,19 @@
+﻿using EnhancedCVAgent.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnhancedCVAgent.Domain.ValueObjects.Company
+{
+    public sealed record CompanyValue
+    {
+        public CompanyValue(string value, string? description) 
+        {
+            if (string.IsNullOrEmpty(value)) throw new DomainValidationException("Company value cannot be empty.");
+            Value = value;
+            Description = description;
+        }
+        public string Value { get; }
+        public string? Description { get; }
+    }
+}
