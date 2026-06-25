d2aaae7c03d0864ae1ef0d9a7a349741e9141859
joaocarlos-dev <joaohenriquejc@hotmail.com>
2026-06-25 10:32:02 -0300

    feat: add company tone

---
+++ src/EnhancedCVAgent.Domain/ValueObjects/Company/CompanyTone.cs
@@ -0,0 +1,21 @@
+﻿using EnhancedCVAgent.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnhancedCVAgent.Domain.ValueObjects.Company
+{
+    public sealed record CompanyTone
+    {
+        public CompanyTone(string tone, string? description)
+        {
+            if (string.IsNullOrEmpty(tone))
+                throw new DomainValidationException("Company tone name cannot be empty.");
+
+            Tone = tone;
+            Description = description;
+        }
+        public string Tone { get; }
+        public string? Description { get; }
+    }
+}
