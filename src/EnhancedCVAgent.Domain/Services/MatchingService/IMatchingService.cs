f0941b436e9f7b91d852c30095f956371b8dcac4
joaocarlos-dev <joaohenriquejc@hotmail.com>
2026-07-06 16:07:38 -0300

    chore: change matching service location

--- src/EnhancedCVAgent.Domain/Services/MatchingService/IMatchingService.cs
+++ src/EnhancedCVAgent.Domain/Services/MatchingService/IMatchingService.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace EnhancedCVAgent.Domain.Services
+namespace EnhancedCVAgent.Domain.Services.MatchingService.MatchingService
 {
     public interface IMatchingService
     {
---
+++ src/EnhancedCVAgent.Domain/Services/MatchingService/MatchingService.cs
@@ -0,0 +1,27 @@
+﻿using EnhancedCVAgent.Domain.Entities;
+using EnhancedCVAgent.Domain.Exceptions;
+using EnhancedCVAgent.Domain.ValueObjects.Matching;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnhancedCVAgent.Domain.Services.MatchingService
+{
+    public sealed class MatchingService: IMatchingService
+    {
+        public MatchingReport Match(CandidateProfile candidate, JobOpportunity jobOpportunity)
+        {
+            if(candidate is null)
+            {
+                throw new DomainValidationException("Candidate profile cannot be null or empty.");
+            }
+
+            if(jobOpportunity is null)
+            {
+                throw new DomainValidationException("JobOpportunity cannot be null or empty.");
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
