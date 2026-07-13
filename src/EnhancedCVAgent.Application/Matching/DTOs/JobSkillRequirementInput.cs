7913a7262629bbdea63b3cd6dac4a3e84ebc6646
joaocarlos-dev <joaohenriquejc@hotmail.com>
2026-07-13 15:31:30 -0300

    feat: add JobSkillRequirement Dto

---
+++ src/EnhancedCVAgent.Application/Matching/DTOs/JobSkillRequirementInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnhancedCVAgent.Application.Matching.DTOs
+{
+    public class JobSkillRequirementInput
+    {
+        public JobSkillRequirementInput(string skill, string? skillExperienceTime, RequirementType requirementType)
+        {
+            Skill = skill;
+            SkillExperienceTime = skillExperienceTime;
+            RequirementType = requirementType;
+        }
+        public string Skill {  get; set; }
+        public string? SkillExperienceTime { get; set; }
+        public RequirementType RequirementType { get; set; }
+    }
+}
