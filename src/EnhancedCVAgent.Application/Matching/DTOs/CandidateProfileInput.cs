79d62a81a36483f864ef8589b5ff521276b4285b
joaocarlos-dev <joaohenriquejc@hotmail.com>
2026-07-13 15:31:04 -0300

    feat: Add CancidadeProfileDto

---
+++ src/EnhancedCVAgent.Application/Matching/DTOs/CandidateProfileInput.cs
@@ -0,0 +1,20 @@
+﻿using EnhancedCVAgent.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnhancedCVAgent.Application.Matching.DTOs
+{
+    public class CandidateProfileInput
+    {
+        public CandidateProfileInput(string professionalSummary, string fullName, List<Skill> skills)
+        {
+            ProfessionalSummary = professionalSummary;
+            FullName = fullName;
+            Skills = skills;
+        }
+        public List<Skill> Skills { get; set; }
+        public string FullName { get; set; }
+        public string ProfessionalSummary { get; set; }
+    }
+}
