d4ed01d70dacbf2c9add31e673ac06374792e95f
joaocarlos-dev <joaohenriquejc@hotmail.com>
2026-07-13 16:11:45 -0300

    feat: add MatchResultDto

---
+++ src/EnhancedCVAgent.Application/Matching/DTOs/MatchResultDto.cs
@@ -0,0 +1,17 @@
+﻿using EnhancedCVAgent.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnhancedCVAgent.Application.Matching.DTOs
+{
+    public class MatchResultDto
+    {
+        public int TotalScore { get; set; }
+        public int TechnicalScore { get; set; }
+        public List<string> MissingRequiredSkills { get; set; }
+        public List<string> MissingPreferredSkills {  get; set; }
+        public List<string> Reasons { get; set; }
+        public ConfidenceLevel Confidence { get; set; }
+    }
+}
