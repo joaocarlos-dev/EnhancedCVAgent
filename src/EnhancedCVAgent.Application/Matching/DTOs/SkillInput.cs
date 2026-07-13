189453979fb09fdc2ba98afba9b55a8ee1a49861
joaocarlos-dev <joaohenriquejc@hotmail.com>
2026-07-13 15:31:47 -0300

    feat: add SkillInput Dto

---
+++ src/EnhancedCVAgent.Application/Matching/DTOs/SkillInput.cs
@@ -0,0 +1,18 @@
+﻿using EnhancedCVAgent.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnhancedCVAgent.Application.Matching.DTOs
+{
+    public class SkillInput
+    {
+        public SkillInput(string name, SkillLevel skillLevel)
+        {
+            Name = name;
+            SkillLevel = skillLevel;
+        }
+        public string Name { get; set; }
+        public SkillLevel SkillLevel { get; set; }
+    }
+}
